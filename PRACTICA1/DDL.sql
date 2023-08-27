06fd73ee2edee4ebea71e13cab74f8ad8aa0ee66
cesarchs <cesarchamalesican@gmail.com>
2023-08-26 23:54:17 -0600

    Se agregaron transacciones iniciales

--- PRACTICA1/DDL.sql
+++ PRACTICA1/DDL.sql
@@ -2,6 +2,29 @@ select * from [practica1].[Course]
 
 -- TRANSACCIONES
 
+-- PR4
+GO
+ CREATE PROCEDURE [practica1].[PR4]
+    @RoleName NVARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Existencia del rol
+    IF NOT EXISTS (SELECT 1 FROM [practica1].[Roles] WHERE [RoleName] = @RoleName)
+    BEGIN
+
+        INSERT INTO [practica1].[Roles] ([Id], [RoleName])
+        VALUES (NEWID(), @RoleName);
+
+        PRINT 'Rol creado exitosamente: ' + @RoleName;
+    END
+    ELSE
+    BEGIN
+        PRINT 'El rol ya existe: ' + @RoleName;
+    END
+END;
+
 -- TRIGGERS
 
 -- FUNCIONES
