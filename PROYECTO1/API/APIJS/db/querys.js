ac526ac635d9d747008db0fcc1e77cf346ca79e0
cesarchs <cesarchamalesican@gmail.com>
2023-09-21 22:24:46 -0600

    se agrego carga de tablas

--- PROYECTO1/API/APIJS/db/querys.js
+++ PROYECTO1/API/APIJS/db/querys.js
@@ -2,4 +2,6 @@ export const querys = {
 
     addCollection: "INSERT INTO collections (id, name, url) VALUES(@id, @name, @url);",
     addGenre: "INSERT INTO genre (id, name, url) VALUES(@id, @name, @url);",
+    addGame_mode: "INSERT INTO game_mode (id, name, url) VALUES(@id, @name, @url);",
+    addFranquicia: "INSERT INTO franchise (id, name, url) VALUES(@id, @name, @url);",
 };
--- PROYECTO1/API/APIJS/prueba.js
+++ PROYECTO1/API/APIJS/prueba.js
@@ -6,7 +6,7 @@ fetch(
         'Client-ID': 'wvy8k49cvhyd1yywjd9ur7hkpl7zoc',
         'Authorization': 'Bearer jk61hgwkq8sagb0fupl2h78assu74j',
       },
-      body: "fields *;"
+      body: "fields *; sort id desc;"
   })
   .then(response => {
     if (!response.ok) {
