8a6d21e2592ed70be832fc5e3ac0147a4c05d01c
Doned-cmd <bryanmendez101@gmail.com>
2023-09-22 13:31:48 -0600

    release dates

--- PROYECTO1/API/APIJS/db/querys.js
+++ PROYECTO1/API/APIJS/db/querys.js
@@ -7,4 +7,5 @@ export const querys = {
     addengine: "INSERT INTO game_engine (id, name, url, description) VALUES(@id, @name, @url, @description);",
     addengine_company: "INSERT INTO game_engine_company (id_engine, id_company) VALUES(@id_engine, @id_company);",
     addengine_platform: "INSERT INTO game_engine_platform (id_engine, id_platform) VALUES(@id_engine, @id_platform);",
+    addrelease_date: "INSERT INTO release_date (id, date, human, platform, region, status, m, y, id_game) VALUES(@id, @date, @human, @platform, @region, @status, @m, @y, @id_game);",
 };
