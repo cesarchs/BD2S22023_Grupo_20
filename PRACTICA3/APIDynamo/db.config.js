b7905fc414e956fccfda897eb9127bd834887ffd
cesarchs <cesarchamalesican@gmail.com>
2023-11-02 00:30:51 -0600

    Se agrego la api de dynamodb
    creacion de tabla, conexion y crud en dynamo

---
+++ PRACTICA3/APIDynamo/db.config.js
@@ -0,0 +1,16 @@
+import AWS from 'aws-sdk'
+
+AWS.config.update({
+    region: "us-east-1",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "AKIA3XBALMIRFNCZ3LWN",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "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"
+})
+
+const db = new AWS.DynamoDB.DocumentClient()
+
+const Table = 'Pelis'
+
+export {
+    db,
+    Table
+}
