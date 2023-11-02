520f127e0050d5d2988da052938c88ee9e64c2de
Doned-cmd <bryanmendez101@gmail.com>
2023-11-02 02:29:05 -0600

    [Add] Load to dbs

---
+++ PRACTICA3/ApiLoad/funcs/funcs.js
@@ -0,0 +1,109 @@
+import { conectarBaseDeDatos, obtenerColeccion, cerrarConexion } from './dbcon.js'
+import { createOrUpdate, deleteUserById, getUserById, readAllUsers } from './dynamofuncs.js'
+
+import fs from 'fs';
+import fastcsv from 'fast-csv';
+
+
+export  async function  test (req, res){
+    console.log('insert - libros');
+    try{
+        await conectarBaseDeDatos();
+
+        //console.log('Conexión a MongoDB establecida');
+        const miColeccion = obtenerColeccion('libroprueba');
+
+        
+
+        const stream = fs.createReadStream('E:/Descargas/load/Libros.csv', 'utf-8');
+        const parser = fastcsv.parse({ headers: true });
+
+        let contadorlibros = 1;
+        const results = [];
+        const autores = [];
+        const categoria = [];
+        stream.pipe(parser)
+        .on('data', async (row) => {
+
+            // Procesa cada fila del archivo CSV aquí
+            if (autores.indexOf(row.Autor) == -1) {
+                autores.push(row.Autor);                
+            }
+            if (categoria.indexOf(row.Categoria) == -1) {
+                categoria.push(row.Categoria);                
+            }
+
+            //console.log(row);
+
+            let  nuevo = {id:results.length, Titulo: row.Titulo,  Descripcion: row.Descripcion, FechaDePublicacion: row.FechaDePublicacion, Calificacion: Number(row.Calificacion),
+            Stock: Number(row.Stock), Precio: parseFloat(row.Precio),
+            autor:{id: autores.indexOf(row.Autor), nombre: row.Autor }, 
+            categoria:{id: categoria.indexOf(row.Categoria), nombre: row.Categoria }};
+
+            results.push(nuevo);            
+            await miColeccion.insertOne(nuevo)
+            //.then((result) => {
+            //     console.log(`Inserted ${result.insertedCount} item(s) with id: ${result.insertedId}`);
+            // }).catch((err) => {
+            //     console.error(`Error al insertar items: ${err}`);
+            // });
+
+            //await contadorlibros++;
+        })
+        .on('end', () => {
+            //console .log('Fin del archivo');
+            res.send(results);
+        });
+
+        //await cerrarConexion();
+    }catch(error){
+        console.error('Error al conectar a MongoDB:', error);
+    }
+
+
+}
+
+export  async function  insert (req, res){
+
+    console.log('insert - peliculas');
+    try{
+
+        const stream = fs.createReadStream('E:/Descargas/load/Peliculas.CSV', 'utf-8');
+        const parser = fastcsv.parse({ headers: true });
+
+
+        const results = [];
+        const Director = [];
+        const Distribuidora = [];
+        stream.pipe(parser)
+        .on('data', async (row) =>{
+
+            // Procesa cada fila del archivo CSV aquí
+            if (Director.indexOf(row.Director) == -1) {
+                Director.push(row.Director);                
+            }
+            if (Distribuidora.indexOf(row.Distribuidora) == -1) {
+                Distribuidora.push(row.Distribuidora);                
+            }
+
+            let  nuevo = {id:results.length, Titulo: row.Titulo, FechaDeEstreno: row.FechaDeEstreno, IdiomaOriginal: row.IdiomaOriginal, Descripcion: row.Descripcion, 
+                Precio: parseFloat(row.Precio), Genero: row.Genero, Calificacion: row.Calificacion, Calificacion: Number(row.Calificacion), Precio2: parseFloat(row.Precio2),
+                director:{id: Director.indexOf(row.Director), nombre: row.Director }, 
+                distribuidora:{id: Distribuidora.indexOf(row.Distribuidora), nombre: row.Distribuidora }};
+
+
+            //await createOrUpdate(JSON.stringify(nuevo))
+            results.push(nuevo);
+            await createOrUpdate(nuevo)
+            
+        })
+        .on('end', async () => {
+            res.send(results);
+            //await createOrUpdate(results)
+        });
+       
+    }catch(error){
+        console.error('Error al conectar a MongoDB:', error);
+    }
+    
+}
